05000abd610e0ed519d61042e4bb6f54a0b19969
chen_junior <593901585@qq.com>
2019-08-17 09:29:21 +0800

    模板及相关资源的添加

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url, include
 
 from . import views
 urlpatterns = [
-    url(r'^', views.index),  #
+    url(r'^', views.index),  # 首页
+    url(r'^news/$', views.news),  # 新闻
+    url(r'^bbs/$', views.bbs),  # 论坛
+
 ]
